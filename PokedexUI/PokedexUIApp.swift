ad3fcc53e5993210f3ff85de15d6fb3e21f7efc5
gallaugher <john.gallaugher@bc.edu>
2022-07-21 21:42:26 -0400

    Initial Commit

---
+++ PokedexUI/PokedexUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexUIApp.swift
+//  PokedexUI
+//
+//  Created by John Gallaugher on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
